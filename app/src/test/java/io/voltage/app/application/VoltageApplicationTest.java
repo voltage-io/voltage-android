9e92f32b34a15ac979e73d5af60aa0a0800aca11
Joshua Winters <winters.118@gmail.com>
2017-10-08 11:35:09 -0400

    Add unit testing environment

---
+++ app/src/test/java/io/voltage/app/application/VoltageApplicationTest.java
@@ -0,0 +1,18 @@
+package io.voltage.app.application;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class VoltageApplicationTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
