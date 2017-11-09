1f06c6c432dc976d99e4440c121f047946c4bcc3
Joshua Winters <winters.118@gmail.com>
2017-11-08 23:28:35 -0500

    Add log levels

--- app/src/main/java/io/voltage/app/utils/Logger.java
+++ app/src/main/java/io/voltage/app/utils/Logger.java
@@ -4,16 +4,43 @@ import android.util.Log;
 
 public class Logger {
 
+    private static int sLevel = Level.DEBUG;
 
-    public static void v(final String message) {
-        Log.v("voltage", message);
+    public interface Level {
+        int NONE = 0, ERROR = 1, WARNING = 2, VERBOSE = 3, INFO = 4, DEBUG = 5;
+    }
+
+    public static void level(final int level) {
+        sLevel = level;
     }
 
     public static void d(final String message) {
-        Log.d("voltage", message);
+        if (sLevel >= Level.DEBUG) {
+            Log.d("voltage", message);
+        }
+    }
+
+    public static void i(final String message) {
+        if (sLevel >= Level.INFO) {
+            Log.d("voltage", message);
+        }
+    }
+
+    public static void v(final String message) {
+        if (sLevel >= Level.VERBOSE) {
+            Log.v("voltage", message);
+        }
+    }
+
+    public static void w(final String message) {
+        if (sLevel >= Level.WARNING) {
+            Log.w("voltage", message);
+        }
     }
 
     public static void ex(final Throwable throwable) {
-        Log.e("voltage", throwable.getLocalizedMessage(), throwable);
+        if (sLevel >= Level.ERROR) {
+            Log.e("voltage", throwable.getLocalizedMessage(), throwable);
+        }
     }
 }
